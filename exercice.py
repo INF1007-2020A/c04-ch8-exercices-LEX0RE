263e4360fe2c2c5aa794f7b3776ab1a06b67ecd5
Alexandre Gélinas <gelinas.alexandre.2001@gmail.com>
2020-10-27 18:33:16 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -22,8 +22,18 @@ def triple_space(input_file, output_file):
         for line in file1:
             file2.write(line.replace(" ", "   "))
 
+def attribute_letter(input_file, output_file):
+    with open(input_file, encoding="utf-8") as in_file, open(output_file, "w", encoding="utf-8") as out_file:
+        for line in in_file:
+            for letter, notes in PERCENTAGE_TO_LETTER.items():
+                note = int(line.strip())
+                if notes[1] > note >= notes[0]:
+                    out_file.write(str(note) + " " + letter + "\n")
+                    break
+
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
     #compare_file("exemple.txt", "notes.txt")
     #triple_space("exemple.txt", "test.txt")
+    attribute_letter("notes.txt", "notes_result.txt")
     pass
